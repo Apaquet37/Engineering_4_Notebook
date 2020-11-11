426ddb5969a8b3c6079a7a9e5fe27863d4028a56
Apaquet37 <apaquet37@charlottesvilleschools.org>
2020-11-11 22:56:05 +0000

    Finished dice clear
    git reset

--- Python/dice_roller.py
+++ Python/dice_roller.py
@@ -1,12 +1,15 @@
 # Automatic Dice Roller
 # Written by: Abby Paquette
 
-import time
-import keyboard
-from random import randint
+import time #importing the time module so that I can use commands like time.sleep
+from random import randint #This is the module that lets me generate a random number, and I specifically imported the part that let me generate an integer
 
-print ("Automatic Dice Roller")
+print ("Automatic Dice Roller") #Printing this is half to tell the user what the program is and half to test and make sure it is running
 
-while input == enter:
-	print(randint(1, 6)
-	time.sleep(1)
+while True:
+	time.sleep(.25) #Pause for 1/4 of a second so the pi doesn't overload from trying to print and process super quickly
+	repeat = input("Press enter to roll or x and enter to quit.") #Instructions are printed for the user, and read as an input
+	if repeat == (""): #If the user presses enter, roll the die
+		print(randint(1,6)) #This prints a random integer between 1 and 6
+	if repeat == ("x"): #If the user presses x and then enter,
+		exit() #Exit the program
