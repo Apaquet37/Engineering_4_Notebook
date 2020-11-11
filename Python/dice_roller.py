c9de678c1bbafb70bfae9d86df42ed2a7202db87
Apaquet37 <apaquet37@charlottesvilleschools.org>
2020-11-11 22:28:35 +0000

    Changes to dice roller, 11/11
    clear

--- Python/dice_roller.py
+++ Python/dice_roller.py
@@ -1,6 +1,12 @@
 # Automatic Dice Roller
 # Written by: Abby Paquette
 
+import time
+import keyboard
 from random import randint
 
 print ("Automatic Dice Roller")
+
+while input == enter:
+	print(randint(1, 6)
+	time.sleep(1)
