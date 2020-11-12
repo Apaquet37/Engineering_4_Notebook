c7fe99e2ac6156467ab0d8c0dc2a71ab89bd86f1
Apaquet37 <apaquet37@charlottesvilleschools.org>
2020-11-12 21:08:47 +0000

    Start of calculator

---
+++ Python/calculator.py
@@ -0,0 +1,16 @@
+#Calculator
+#Written by: Abby Paquette
+
+doMath():
+	t = 1
+	if(t == 1):
+		num = a+b
+	if(t == 2):
+		num = a-b
+	if(t == 3):
+		num = a*b
+	if(t == 4):
+		num = a/b
+	if(t == 5):
+		#num = modulo?
+
