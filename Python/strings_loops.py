24395b9217c1bab63342631971817956c5e84f16
Apaquet37 <apaquet37@charlottesvilleschools.org>
2020-11-30 14:15:31 +0000

    Made strings and loops on repl.it

---
+++ Python/strings_loops.py
@@ -0,0 +1,17 @@
+#Strings and Loops
+#Written by Abby Paquette and Elisabeth Scharf
+
+import numpy as np
+
+txt = input("Type a simple sentence: ")
+
+letters = list(txt)
+
+array1 = np.array(letters)
+k = []
+
+for i in letters:
+    str(i).replace((array1[2]), '-')
+    txt.split(" ")
+    newStr = i.replace(' ', '-')
+    print(newStr)
