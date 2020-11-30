d0c213a108e5cd4a8ca62ffe3426a998d0dd2b25
Apaquet37 <48455283+Apaquet37@users.noreply.github.com>
2020-11-30 10:05:48 -0500

    Add comments

--- Python/strings_loops.py
+++ Python/strings_loops.py
@@ -1,15 +1,12 @@
 #Strings and Loops
 #Written by Abby Paquette and Elisabeth Scharf
 
-import numpy as np
+import numpy as np #This is the library for arrays
 
-txt = input("Type a simple sentence: ")
+txt = input("Type a simple sentence: ") #Prompting the user and accepting input
+letters = list(txt) #Making the input into a list called letters
+array1 = np.array(letters) #Making the list into an array
 
-letters = list(txt)
-
-array1 = np.array(letters)
-k = []
-
-for i in letters:
-    newStr = i.replace(' ', '-')
-    print(newStr)
+for i in letters: #Just looping through enough times for each character in the sentence
+    newStr = i.replace(' ', '-') #Replacing the spaces with dashes
+    print(newStr) #Printing the new sentence that is split up
