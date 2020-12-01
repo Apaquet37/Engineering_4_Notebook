f2d6c028b525085e21249194d1bf9c73bfeacea3
Apaquet37 <apaquet37@charlottesvilleschools.org>
2020-12-01 03:16:53 +0000

    Take out unnecessary lines

--- Python/strings_loops.py
+++ Python/strings_loops.py
@@ -1,11 +1,8 @@
 #Strings and Loops
 #Written by Abby Paquette and Elisabeth Scharf
 
-import numpy as np #This is the library for arrays
-
 txt = input("Type a simple sentence: ") #Prompting the user and accepting input
 letters = list(txt) #Making the input into a list called letters
-array1 = np.array(letters) #Making the list into an array
 
 for i in letters: #Just looping through enough times for each character in the sentence
     newStr = i.replace(' ', '-') #Replacing the spaces with dashes
