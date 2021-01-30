3bc36a4db1c959b33d434ef72d789b42906c605c
Apaquet37 <48455283+Apaquet37@users.noreply.github.com>
2021-01-29 20:05:30 -0500

    Comments for led.sh

--- Python/led.sh
+++ Python/led.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/bin/bash 
+#Allows the script to be run just by calling its name
 
-for i in {1..10}
-do
-	/usr/bin/gpio -1 toggle 40
-	/usr/bin/gpio -1 toggle 37 
-	sleep .5
-	/usr/bin/gpio -1 toggle 40
-	/usr/bin/gpio -1 toggle 37
-	sleep .5
-done
+for i in {1..10} #For loop that runs ten times
+do #Bash specific syntax
+	/usr/bin/gpio -1 toggle 40 #Toggles pin 40 (turning it on)
+	/usr/bin/gpio -1 toggle 37 #Toggles pin 37
+	sleep .5 #Pause for half a second so you can notice the blink
+	/usr/bin/gpio -1 toggle 40 #Toggles pin 40 (turning it off)
+	/usr/bin/gpio -1 toggle 37 #Toggles pin 47
+	sleep .5 #Pause for half a second
+done #Bash specific syntax
