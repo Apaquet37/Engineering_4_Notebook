831ed47d904cc593026f72cb455fa8e8acb904c4
Apaquet37 <apaquet37@charlottesvilleschools.org>
2021-01-30 01:13:55 +0000

    Fixed a comment

--- Python/led.sh
+++ Python/led.sh
@@ -7,6 +7,6 @@ do #Bash specific syntax
 	/usr/bin/gpio -1 toggle 37 #Toggles pin 37
 	sleep .5 #Pause for half a second so you can notice the blink
 	/usr/bin/gpio -1 toggle 40 #Toggles pin 40 (turning it off)
-	/usr/bin/gpio -1 toggle 37 #Toggles pin 47
+	/usr/bin/gpio -1 toggle 37 #Toggles pin 37
 	sleep .5 #Pause for half a second
 done #Bash specific syntax
