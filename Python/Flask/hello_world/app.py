1186d320f19d92d63b0601e283fbc00c52132bbe
Apaquet37 <48455283+Apaquet37@users.noreply.github.com>
2021-03-15 10:06:17 -0400

    Comment app.py

--- Python/Flask/hello_world/app.py
+++ Python/Flask/hello_world/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) #Making a new flask app called "app"
 
-@app.route("/")
-def hello_world():
-     return "hello world!"
+@app.route("/") #Any requests to the root directory (hello_world) should fire off the hello_world() function
+def hello_world(): #The function
+     return "hello world!" #Says "hello world!"
 
-if __name__ == "__main__":
-     app.run(host="0.0.0.0", port=80)
+if __name__ == "__main__": 
+     app.run(host="0.0.0.0", port=80) #Runs the app, host="0.0.0.0" allows the app to be visible to other computers
